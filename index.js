b592e27c46d592fd57193e736686b14084f7430b
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-01-14 14:47:04 -0500

    commit tree d58fb84ac69787352be93f6602c02d7b902c69de

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+let destinationURL = 'http://localhost:3000/users'
+
+let formData = {
+   dogName: "Byron",
+   dogBreed: "Poodle"
+}; 
+
+let configurationObject = {
+   method: "POST", 
+   headers: {
+      "Content-Type": "application/json", 
+      "Accept": "application/json"
+   }, 
+   body: JSON.stringify(formData)
+}
+
+fetch(destinationURL, configurationObject)
+   .then(function(response) {
+      return response.json();
+   })
+   .then(function(object) {
+      console.log(object);
+   })
+   .catch(function (error) {
+      alert("Bad things! Ragnarok!"); 
+      console.log(error.message);
+   });
